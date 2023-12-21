c60e11581e82b76530d98158787e496a7e5fcc63
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2023-12-20 19:55:50 -0500

    Create localexample.tf

---
+++ localexample.tf
@@ -0,0 +1,23 @@
+locals {​
+
+  service_name = "forum"​
+
+  owner        = "Community Team"​
+
+}​
+
+Output variables Example:​
+
+​
+
+output "print"{​
+
+      value = local.service_name​
+
+}​
+
+output "secondprint"{​
+
+      value = local.owner​
+
+}​
