8aa7066b67c9347194b48b0c8fdbe14480419101
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2023-12-22 19:24:49 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "mcit420zz5um" {
   name     = "database-rg"
   location = "West Europe"
 }
 
 resource "azurerm_mssql_server" "example" {
   name                         = "mssqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+  resource_group_name          = azurerm_resource_group.mcit420zz5um.name
+  location                     = azurerm_resource_group.mcit420zz5um.location
   version                      = "12.0"
   administrator_login          = var.db_login
   administrator_login_password = var.db_password
