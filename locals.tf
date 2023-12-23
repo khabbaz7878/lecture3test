391fa692a486364cae0090484c419a969097cdf1
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2023-12-22 20:28:10 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -6,4 +6,11 @@ locals {
   owner = "Community Team"
   lengthofservice_name=length(local.service_name)
 favoritclublist=["RealMadrid","Alhilal","ACMILAN"]
+
+locals {
+  num1 = tonumber(var.numbers[0])
+  num2 = tonumber(var.numbers[1]) 
+  num3 = tonumber(var.numbers[2])
+  
+  sum = local.num1 + local.num2 + local.num3
 }
