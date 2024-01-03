1d707a223208b2d8ee065df07d57af716fa86c17
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 17:02:58 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -8,12 +8,3 @@ locals {
 favoritclublist=["RealMadrid","Alhilal","ACMILAN"]
 
 }
-locals {
-  storage_account_names = [
-    "${var.first_name}mcit${var.last_name}1", 
-    "${var.first_name}mcit${var.last_name}2",
-    "${var.first_name}mcit${var.last_name}3",
-    "${var.first_name}mcit${var.last_name}4",
-    "${var.first_name}mcit${var.last_name}5"
-  ]
-}
