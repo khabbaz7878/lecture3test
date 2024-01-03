9fdf645b5a21fd629731b0a97fad1c5dc6cf8c49
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-03 17:03:26 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -20,19 +20,4 @@ resource "azurerm_mssql_server" "example" {
   tags = {
     environment = "production"
   }
-}
 
-resource "azurerm_storage_account" "storage" {
-  count = length(local.storage_account_names)
-  
-  name                     = local.storage_account_names[count.index]
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location  
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "storage-rg"
-  location = "eastus"
-}
