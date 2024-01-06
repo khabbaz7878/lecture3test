5d3b244cd2100b542c6fe72d4d31fc88387af631
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-05 20:37:43 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -8,3 +8,11 @@ locals {
 favoritclublist=["RealMadrid","Alhilal","ACMILAN"]
 
 }
+locals{
+  servicename="abracadabra"
+  forum="abracadabramcit"
+  lengthsa=length(local.servicename)
+  lengthforum=length(local.forum)
+  winterlistOfSports=["icehockey","snowboarding","iceskating"]
+  total_output = ["150", "150", "150"]
+}
