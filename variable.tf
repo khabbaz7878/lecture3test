a284fde194eb22e335048e7600acb1fcb9b274df
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-08 20:06:16 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -115,7 +115,7 @@ locals {
  characters = ["luke", "yoda", "darth"]
  enemies_destroyed = [4252, 900, 20000056894]
  map = {
-    for index, character in toset(local.characters):
+    for index, character in toset(local.characters)
       character = local.enemies_destroyed[index]
  }
 }
