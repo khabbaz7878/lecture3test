53a39cb72971b9e07414bcee2d000c1977c930b4
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-10 19:50:36 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -22,5 +22,8 @@ locals {
  local_flattened_list = flatten(var.nested_list)
 }
 
+locals {
+ flattened_map = flatten([for k, v in var.nested_map : [for kk, vv in v: {group=k, key = kk, value = vv }]])
+}
 
 
