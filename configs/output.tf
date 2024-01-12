d2dd7b2702431ba9e256c29e298db19a516b2572
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-12 18:29:48 -0500

    Update output.tf

--- configs/output.tf
+++ configs/output.tf
@@ -1,1 +1,19 @@
 
+
+linux_app_list = [
+ {
+    "name" = "my_app_1"
+    "os_type" = "Linux"
+    "sku_name" = "S1"
+ },
+ {
+    "name" = "my_app_2"
+    "os_type" = "Linux"
+    "sku_name" = "S2"
+ },
+ {
+    "name" = "my_app_3"
+    "os_type" = "Linux"
+    "sku_name" = "S3"
+ },
+]
