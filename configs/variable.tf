24baa4e513fe840b02fb27427b665a2cf64b23eb
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-12 19:04:17 -0500

    Delete configs/variable.tf

--- configs/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "linux_app_list" {
- description = "List of Linux applications."
- type        = list(object({
-    name        = string
-    os_type     = string
-    sku_name    = string
- }))
-}
-
