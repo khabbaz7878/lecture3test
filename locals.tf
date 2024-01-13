86f64b938467b6d4e767478cc23e9dba99c9b2c9
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-12 19:10:34 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -32,19 +32,4 @@ locals {
  my_value = lookup(var.my_map, "key1", "")
 }
 
-locals {
-  linux_app = [
-    for f in fileset("${path.moduleconfigs}", "*.yaml") :
-    yamldecode(file("${path.module}/configs}"))
-  ]
 
-  linux_app_list = flatten([
-    for app in local.linux_app : [
-      for linuxapps in try(app.listoflinuxapp, []) : {
-        name        = linuxapps.name
-        os_type     = linuxapps.os_type
-        sku_name    = linuxapps.sku_name
-      }
-    ]
-  ])
-}
