16caef97763f920099eaeb0a118b1af440e38f49
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-12 19:26:42 -0500

    Update readyyaml_linuxapp.tf

--- readyyaml_linuxapp.tf
+++ readyyaml_linuxapp.tf
@@ -15,7 +15,7 @@ resource "azurerm_service_plan" "batcha06sp" {
  name                = each.value.name
  location            = each.value.location
  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
