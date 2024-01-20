f1e87e69a80d7af3caaeca19ab9477896b97720b
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-19 21:51:44 -0500

    Create storageaccount.tf

---
+++ windows_function_app/storageaccount.tf
@@ -0,0 +1,6 @@
+storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+ 
+  site_config {}
+}
