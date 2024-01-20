92576f4c75f6fdbb491ecb77293955382cfd33b1
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-19 21:30:57 -0500

    Update windows_function.tf

--- windows_function_app/windows_function.tf
+++ windows_function_app/windows_function.tf
@@ -1,7 +1,11 @@
-provider "azurerm" {
-  features {}
-}
 
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
