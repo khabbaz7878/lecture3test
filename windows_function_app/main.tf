85937b04d3f5ec51a034003f66959448abf66973
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-19 21:50:04 -0500

    Update main.tf

--- windows_function_app/main.tf
+++ windows_function_app/main.tf
@@ -1,7 +1,3 @@
-module "windowsfuction_app" {
-  source   = "./windows_function_app"
-  name= var.wfaname
-  account_tier= var.account_tier
-  account_replication_type= var.account_replication_type
-  os_type= var.os_type
-  sku_name= var.os_type
+provider "azurerm" {
+  features = {}
+}
