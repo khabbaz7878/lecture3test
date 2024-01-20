3b682051955c1044c76621d31ef083b154fae736
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-19 21:14:36 -0500

    Update main.tf

--- windows_function_app/main.tf
+++ windows_function_app/main.tf
@@ -1,33 +1,7 @@
-resource "azurerm_resource_group" "exam_example" {
-  name     = "exam_example-rg"
-  location = "Canada East"
-}
-
-
-resource "azurerm_storage_account" "sa_exam_example" {
-  name                     = "windowsfunctionappsa"
-  resource_group_name      = azurerm_resource_group.exam_example.name
-  location                 = azurerm_resource_group.exam_example.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-}
-
-resource "azurerm_service_plan" "sp_exam_example" {
-  name                = "example-app-service-plan"
-  resource_group_name = azurerm_resource_group.exam_example.name
-  location            = azurerm_resource_group.exam_example.location
-  os_type             = var.os_type
-  sku_name            = var.sku_name
-}
-
-resource "azurerm_windows_function_app" "windows_function_app_exam" {
-  name                = var.wfaname
-  resource_group_name = azurerm_resource_group.exam_example.name
-  location            = azurerm_resource_group.exam_example.location
-
-  storage_account_name       = azurerm_storage_account.sa_exam_example.name
-  storage_account_access_key = azurerm_storage_account.sa_exam_example.primary_access_key
-  service_plan_id            = azurerm_service_plan.sp_exam_example.id
-
-  site_config {}
-}
+module "windowsfuction_app" {
+  source   = "./modules/windowsfunctionapp"
+  name= var.wfaname
+  account_tier= var.account_tier
+  account_replication_type= var.account_replication_type
+  os_type= var.os_type
+  sku_name= var.os_type
