3ac1caed27d5f6f97134cf123cae564e10080d5a
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-19 20:58:31 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,12 +9,4 @@ terraform{
   required_version=">=1.4.0"#this version is for Terraform Version, NOT azurerm
 }
 
-provider "azurerm"{
-  features{}
-  skip_provider_registration="true"
-  
-  subscription_id=var.subscription_id
-  client_id=var.client_id
-  client_secret=var.client_secret
-  tenant_id=var.tenant_id
-}
+
