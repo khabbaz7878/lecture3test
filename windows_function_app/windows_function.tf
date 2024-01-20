54d9d44b686de389e91579c5d0d3d62dff261605
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-19 21:28:58 -0500

    Update windows_function.tf

--- windows_function_app/windows_function.tf
+++ windows_function_app/windows_function.tf
@@ -1,51 +1,48 @@
-resource "azurerm_resource_group" "exam_example" {
-  name     = "exam_example-rg"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "West Europe"
 }
 
-locals{
-  windows_function_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
-  windows_fa_list = flatten([
-    for app in local.windows_function_app : [
-      for windows_app in try(app.windowsfunctionapp, []) :{
-        name=windows_app.name
-        account_tier=windows_app.account_tier
-        account_replication_type=windows_app.account_replication_type
-        os_type=windows_app.os_type
-        sku_name=windows_app.sku_name     
-      }
-    ]
-])
+resource "azurerm_storage_account" "example" {
+  name                     = "windowsfunctionappsa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_account" "sa_exam_example" {
-  for_each                  ={for fa in local.windows_fa_list: "${fa.name}"=>fa }
-  name                     = each.value.name
-  resource_group_name      = azurerm_resource_group.exam_example.name
-  location                 = azurerm_resource_group.exam_example.location
-  account_tier             = each.value.account_tier
-  account_replication_type = each.value.account_replication_type
+resource "azurerm_service_plan" "example" {
+  name                = "example-app-service-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_service_plan" "sp_exam_example" {
-  for_each = azurerm_storage_account.sa_exam_example
-  name                = each.value.name
-  resource_group_name = azurerm_resource_group.exam_example.name
-  location            = azurerm_resource_group.exam_example.location
-  os_type             = each.value.os_type
-  sku_name            = each.value.sku_name
+data "yaml" "config" {
+  content = <<YAML
+windowsfunctionapp:
+  - name: examplewindowsfunctionapp
+    account_tier: Standard
+    account_replication_type: LRS
+    os_type: Windows
+    sku_name: Y1
+YAML
+
 }
 
-resource "azurerm_windows_function_app" "windows_function_app_exam" {
-  for_each = azurerm_storage_account.sa_exam_example
-  name                = each.value.name
-  resource_group_name = azurerm_resource_group.exam_example.name
-  location            = azurerm_resource_group.exam_example.location
+resource "azurerm_windows_function_app" "example" {
+  name                = data.yaml.config.windowsfunctionapp[0].name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 
-  storage_account_name       = azurerm_storage_account.sa_exam_example.name
-  storage_account_access_key = azurerm_storage_account.sa_exam_example.primary_access_key
-  service_plan_id            = each.value.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
 
   site_config {}
 }
-
